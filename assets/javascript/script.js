7d83abaabea8322a70f6a981b9405a1df1b3d47c
Maki Hirose <Capture@dyn-129-236-212-27.dyn.columbia.edu>
2019-02-23 09:41:52 -0500

    190223 9:41am Commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,17 +6,23 @@ var myHtmlBody = document.getElementsByTagName("body")[0];
 
 var animal = ["Dog", "Cat", "Horse", "Lion", "Tiger", "Wolf"];     // An array of animals
 
-
-// Create a for loop for the length of the array to display the different animals as buttons
-for (i = 0; i < animal.length; i++) {                                                  // The for loop will repeat the length of the animals array
+for ( var i = 0; i < animal.length; i++){
     var btn = document.createElement("button");                                        // Create a <button element>
     btn.innerHTML = animal[i];                                                         // Name the button with animal name
-    myHtmlBody.appendChild(btn);                                                       // Add the button to the HTML body
-    btn.addEventListener ("click", function() {                                        // Add a "click" event listener to the button
-    
+    myHtmlBody.appendChild(btn);       
+    btn.addEventListener("click", test);
+}
+// Create a for loop for the length of the array to display the different animals as buttons
+function test(){ 
+//for (var i = 0; i < animal.length; i++) {                                                  // The for loop will repeat the length of the animals array
+    // var btn = document.createElement("button");                                        // Create a <button element>
+    // btn.innerHTML = animal[i];                                                         // Name the button with animal name
+    // myHtmlBody.appendChild(btn);                                                       // Add the button to the HTML body
+    // btn.addEventListener ("click", function() {                                        // Add a "click" event listener to the button
+        console.log(this);
         // Use jQuery from here:
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal[i] + "&api_key=9rqYZE6HplBHPqKeUBfUFVboSn02keof";
+        this.textContent + "&api_key=9rqYZE6HplBHPqKeUBfUFVboSn02keof";
 
         $.ajax({
         url: queryURL,
@@ -44,10 +50,7 @@ for (i = 0; i < animal.length; i++) {                                           
 
             }
 
-        });
-    
-    });     
-
+        });    
 }
 
 
